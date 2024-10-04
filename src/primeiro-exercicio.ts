3019327754bed856552f8335b014a5c6beb85093
darlison-calm <bernardo.darlison@gmail.com>
2024-10-04 13:50:53 -0300

    fix: função countvowels conta vogais com acento

--- src/primeiro-exercicio.ts
+++ src/primeiro-exercicio.ts
@@ -1,16 +1,24 @@
-function countVowal(text: string): number {
-  const matches = text.match(/[aeiou]/gi);
-  
-  return matches ? matches.length : 0;
+/**
+* countVowels
+*
+* Retorna a quantidade de vogais em um texto fornecido.
+*
+* @param text - A string onde as vogais serão contadas.
+* @returns O número de vogais no texto.
+*/
+
+function countVowels(text: string): number {
+  const regex = /[aeiouáéíóúâêôàãõü]/gi;
+  return text.match(regex)?.length || 0
 }
 
 const form = document.getElementById('contar-vogais-form') as HTMLFormElement
 const result = document.querySelector('#result') as HTMLElement;
 const word = document.getElementById('word-input') as HTMLInputElement;
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', (event : Event) => {
   event.preventDefault();
-  const vowelCount = countVowal(word.value);
+  const vowelCount = countVowels(word.value);
   result.textContent = `A palavra ${word.value} tem ${vowelCount} vogais.`;
 })
 
--- src/segundo-exercicio.ts
+++ src/segundo-exercicio.ts
@@ -1,10 +1,11 @@
+
+
 export let lista : Array<Object> = [
   {"id" : 1, "name": "Ada Lovelace", "bio" : "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina"},
   {"id" : 2, "name": "Alan Turing", "bio" : "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial"},
   {"id" : 3, "name": "Nikola Tesla", "bio" : "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada."},
   {"id" : 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar."}
 ]
-
 
 //a) Crie uma função que retorne a bio do id passado
 export function acharBio(id: number) :string {
