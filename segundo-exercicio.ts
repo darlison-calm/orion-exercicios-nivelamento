2256190560d07ba11b388575d0e2fa7045634eb8
darlison-calm <bernardo.darlison@gmail.com>
2024-10-04 16:46:30 -0300

    refactor: tratamento se pessoa não existe

--- segundo-exercicio.ts
+++ segundo-exercicio.ts
@@ -11,90 +11,94 @@ let lista: Array <Pessoa> = [
   {"id" : 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar."}
 ]
 
+function acharPessoa(id: number, lista: Array<Pessoa>) :Pessoa {
+  return lista.find(p => p.id === id)
+}
+
 //a) Crie uma função que retorne a bio do id passado
-function acharBioFuncional(id: number): string {
-  const pessoa = lista.find(p => p.id === id);
-  
-  if(!pessoa) {
+function acharBioFuncional(id: number, lista: Array<Pessoa>): string {
+  const pessoa = acharPessoa(id, lista)
+
+  if (!pessoa) {
     return "Biografia não encontrada pois id não existe"
   }
 
-  if(pessoa.bio === null || pessoa.bio === undefined || pessoa.bio.trim() === '') {
+  if (pessoa.bio === null || pessoa.bio === undefined || pessoa.bio.trim() === '') {
     return "Biografia não esta disponível"
   }
   
   return pessoa.bio
 };
 
-// function acharBioFuncional(id: number, items :Array<Pessoa>) :string {
-//   const ip= items.find(item => item.id === id)
-//   return item || "Biografia não encontrada pois id não existe"
-// }
+function acharBioImperativa(id: number): string {
+ const pessoa = acharPessoa(id, lista)
+  
+  if (!pessoa) {
+    return "Biografia não encontrada pois id não existe"
+  }
+
+  if (pessoa.bio === null || pessoa.bio === undefined || pessoa.bio.trim() === '') {
+    return "Biografia não esta disponível"
+  } 
+  
+  return pessoa.bio
+};
+
 
 //b) Crie uma função que retorne o name do id passado
-function acharNome(id: number) :string {
-  let pessoaNome : string;  
-  
-  lista.forEach(pessoa => {
-    if (pessoa['id'] === id) {
-      pessoaNome = pessoa['name']
-    }
-  });
+function acharNomeFuncional(id: number, lista: Array<Pessoa>) :string {
+  const pessoa = acharPessoa(id, lista)
 
-  return pessoaNome || 'Nome não encotrado pois id não existe';
+  if (!pessoa) {
+    return "Biografia não encontrada pois id não existe"
+  }
+
+  if (pessoa.name === null || pessoa.name === undefined || pessoa.name.trim() === '') {
+    return "Biografia não esta disponível"
+  }
+  
+  return pessoa.name
+}
+
+function acharNomeImperativa(id: number) :string {
+  const pessoa = acharPessoa(id, lista)
+
+  if (!pessoa) {
+    return "Biografia não encontrada pois id não existe"
+  }
+
+  if (pessoa.name === null || pessoa.name === undefined || pessoa.name.trim() === '') {
+    return "Biografia não esta disponível"
+  }
+  
+  return pessoa.name
 }
 
 //c) Crie uma função que apague um item da lista a partir de um id passado
-function apagarItem(id: number): void {
-  let index : number;
-  for (let i = 0; i < lista.length; i++) {
-    
-    if(lista[i]['id'] === id){
-      index = i
-    }
+function apagarItemImperativo(id: number): void {
+  const index: number = lista.findIndex(pessoa => pessoa.id === id)
+
+  if (index !== undefined) {
+    lista.splice(index, 1);
   }
-
-  lista.splice(index, 1)
 }
 
-//d) Crie uma função que altere a bio ou o name a partir de um id passado
-function alterarItem(id: number, campo: 'bio' | 'name', novoValor: string): void {
-  let index: number = lista.findIndex(item => item['id'] === id);
-  lista[index][campo] = novoValor
-}
-
-//e) Demonstre todas as funções com o paradigma funcional e com o imperativo
-function apagarItemFuncional(id: number, items: Array<Object>) :Array <Object> {
-  let index: number = items.findIndex(item => item['id'] === id);
+function apagarItemFuncional(id: number, items: Array<Pessoa>) :Array <Pessoa> {
+  const index: number = items.findIndex(item => item.id === id);
 
   const novaLista = [...items.slice(0, index), ...items.slice(index+1, items.length)]
   return novaLista
 }
-function apagarItemImperativo(id: number): void {
-  let index :number;
-  for (let i = 0; i < lista.length; i++) {
-    if(lista[i]['id'] === id){
-      index = i
-    }
-  }
- 
-  lista.splice(index, 1)
-}
 
+//d) Crie uma função que altere a bio ou o name a partir de um id passado
 function alterarItemImperativo(id: number, campo: 'bio' | 'name', novoValor: string): void {
-  let index: number;
-  for (let i = 0; i < lista.length; i++) {
-    if (lista[i]['id'] === id){
-      index = i
-    }
-  }
+  let index: number = lista.findIndex(item => item.id === id);
   lista[index][campo] = novoValor
 }
 
-
-function alterarItemFuncional(id: number, campo: 'bio' | 'name', novoValor: string, items:Array<Object>):Array<Object> {
-  let novaLista = [...items]
-  let index: number = novaLista.findIndex(item => item['id'] === id);
+function alterarItemFuncional(id: number, campo: 'bio' | 'name', novoValor: string, lista: Array<Pessoa>): Array<Pessoa> {
+  const novaLista = [...lista]
+  let index: number = novaLista.findIndex(pessoa => pessoa.id === id);
   novaLista[index][campo] = novoValor
   return novaLista
 }
@@ -102,22 +106,9 @@ function alterarItemFuncional(id: number, campo: 'bio' | 'name', novoValor: stri
 
 
 
-function acharNomeImperativo(id: number) :string {
-  let pessoaNome : string;  
-  
-  lista.forEach(pessoa => {
-    if (pessoa['id'] === id) {
-      pessoaNome = pessoa['name']
-    }
-  });
 
-  return pessoaNome || 'Nome não encotrado pois id não existe';
-}
 
-function acharNomeFuncional(id: number, items :Array<Object>) :string {
-  const pessoa = items.find(item => item['id'] == id)
-  return pessoa['name'] || 'Nome não encotrado pois id não existe';
-}
+
 
 
 
