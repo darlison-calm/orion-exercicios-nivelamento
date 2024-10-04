5cfc26222795ded5ef1bc7b1c94cd317cfc1d629
darlisonDev <bernardo.darlison@gmail.com>
2024-10-04 20:13:59 -0300

    Adicionar documentação

--- src/exercicio.ts
+++ src/exercicio.ts
@@ -1,8 +1,15 @@
-function countVowal(text: string): number {
-  const matches = text.match(/[aeiou]/gi);
-  
-  return matches ? matches.length : 0;
-}
+/**
+* findVowels
+*
+* Retorna a quantidade de vogais em um texto fornecido.
+*
+* @param text - A string onde as vogais serão contadas.
+* @returns O número de vogais no texto.
+*/
+export function countVowels(text: string): number {
+  const regex = /[aeiouáéíóúâêôàãõü]/gi;
+  return text.match(regex)?.length || 0;
+  }
 
 const form = document.getElementById('form') as HTMLFormElement
 const result = document.querySelector('p') as HTMLElement;
@@ -10,7 +17,7 @@ const word = document.getElementById('word-input') as HTMLInputElement;
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const vowelCount = countVowal(word.value);
+  const vowelCount = countVowels(word.value);
   result.textContent = `A palavra ${word.value} tem ${vowelCount} vogais.`;
 })
 
